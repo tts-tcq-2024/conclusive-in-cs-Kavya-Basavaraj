13042a3fa6417d439605376847da60b465509af9
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 09:30:58 +0530

    Update TypeWiseAlert.Tests.cs

--- TypeWiseAlert.Tests.cs
+++ TypeWiseAlert.Tests.cs
@@ -5,7 +5,9 @@ public class TypeWiseAlertTests
     [Theory]
     [InlineData(TypeWiseAlert.CoolingType.PASSIVE_COOLING, 20, TypeWiseAlert.BreachType.NORMAL)]
     [InlineData(TypeWiseAlert.CoolingType.PASSIVE_COOLING, -1, TypeWiseAlert.BreachType.TOO_LOW)]
-   
+    [InlineData(TypeWiseAlert.CoolingType.PASSIVE_COOLING, 36, TypeWiseAlert.BreachType.TOO_HIGH)]
+    [InlineData(TypeWiseAlert.CoolingType.HI_ACTIVE_COOLING, 46, TypeWiseAlert.BreachType.TOO_HIGH)]
+    [InlineData(TypeWiseAlert.CoolingType.MED_ACTIVE_COOLING, 41, TypeWiseAlert.BreachType.TOO_HIGH)]
     public void ClassifyTemperatureBreach_ReturnsCorrectBreachType(TypeWiseAlert.CoolingType coolingType, double temperature, TypeWiseAlert.BreachType expectedBreach)
     {
         var result = TypeWiseAlert.ClassifyTemperatureBreach(coolingType, temperature);
@@ -20,5 +22,11 @@ public class TypeWiseAlertTests
         // Validate console output, possibly using a custom TextWriter
     }
 
-   
+    [Fact]
+    public void CheckAndAlert_SendsToEmail()
+    {
+        var batteryChar = new TypeWiseAlert.BatteryCharacter { coolingType = TypeWiseAlert.CoolingType.PASSIVE_COOLING, brand = "BrandX" };
+        TypeWiseAlert.CheckAndAlert(TypeWiseAlert.AlertTarget.TO_EMAIL, batteryChar, 36);
+        // Validate console output, possibly using a custom TextWriter
+    }
 }
