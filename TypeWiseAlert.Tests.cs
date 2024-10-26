d7c809d080b50251df9cdec8a055c94a128c5546
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 09:20:02 +0530

    Update TypeWiseAlert.Tests.cs

--- TypeWiseAlert.Tests.cs
+++ TypeWiseAlert.Tests.cs
@@ -1,8 +1,13 @@
-using System;
 using Xunit;
 
 public class TypeWiseAlertTests
 {
-
-
+    [Theory]
+    [InlineData(TypeWiseAlert.CoolingType.PASSIVE_COOLING, 20, TypeWiseAlert.BreachType.NORMAL)]
+   
+    public void ClassifyTemperatureBreach_ReturnsCorrectBreachType(TypeWiseAlert.CoolingType coolingType, double temperature, TypeWiseAlert.BreachType expectedBreach)
+    {
+        var result = TypeWiseAlert.ClassifyTemperatureBreach(coolingType, temperature);
+        Assert.Equal(expectedBreach, result);
+    }
 }
