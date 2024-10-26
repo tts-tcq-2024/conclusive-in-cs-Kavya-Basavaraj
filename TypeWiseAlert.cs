1e1ee2a13f150691bba719490c2f244c895db7fa
Kavya-Basavaraj <132560975+Kavya-Basavaraj@users.noreply.github.com>
2024-10-26 09:26:45 +0530

    Update TypeWiseAlert.cs

--- TypeWiseAlert.cs
+++ TypeWiseAlert.cs
@@ -10,6 +10,39 @@ public class TypeWiseAlert
         TOO_HIGH
     }
 
+    public enum CoolingType
+    {
+        PASSIVE_COOLING,
+        HI_ACTIVE_COOLING,
+        MED_ACTIVE_COOLING
+    }
+
+    public enum AlertTarget
+    {
+        TO_CONTROLLER,
+        TO_EMAIL
+    }
+
+    public struct BatteryCharacter
+    {
+        public CoolingType coolingType;
+        public string brand;
+    }
+
+    public interface IAlertSender
+    {
+        void Send(BreachType breachType);
+    }
+
+    public class ControllerAlert : IAlertSender
+    {
+        public void Send(BreachType breachType)
+        {
+            const ushort header = 0xfeed;
+            Console.WriteLine($"{header} : {breachType}\n");
+        }
+    }
+
     public class EmailAlert : IAlertSender
     {
         private readonly string recepient = "a.b@c.com";
@@ -44,5 +77,4 @@ public class TypeWiseAlert
     }
 
   
-
 }
